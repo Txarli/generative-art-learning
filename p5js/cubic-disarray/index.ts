5d4d96e5dd103215d0d45bfad3648cddf06eb4d0
Aitor Urrutia <aitor.urrutia@biko2.com>
2021-01-15 21:59:58 +0100

    quita var y const

--- p5js/cubic-disarray/index.ts
+++ p5js/cubic-disarray/index.ts
@@ -1,6 +1,6 @@
 import P5 from 'p5'
 
-var multiplier = 0.77;
+const multiplier = 0.77;
 
 const sketch = (p5: P5) => {
 	p5.setup =  () => {
@@ -14,15 +14,15 @@ const sketch = (p5: P5) => {
 	p5.draw = () => {
         p5.noFill();
         p5.strokeWeight(2);
-        var size = 50;
-        var across = 9;
-        var down = 13;
-        var rotateMult = 3;
-        for (var x = 0; x < across; x++) {
-          for (var y = 0; y < down; y++) {
+        const size = 50;
+        const across = 9;
+        const down = 13;
+        const rotateMult = 3;
+        for (let x = 0; x < across; x++) {
+          for (let y = 0; y < down; y++) {
             //without plusMinus all the squares will rotate in one direction
-            var plusMinus = p5.random() > .5 ? 1 : -1;
-            var rotateAmt = y * plusMinus * p5.random() * rotateMult * multiplier;
+            const plusMinus = p5.random() > .5 ? 1 : -1;
+            const rotateAmt = y * plusMinus * p5.random() * rotateMult * multiplier;
             p5.push();
             p5.translate(x*size+size, y*size+size);
             p5.rotate(rotateAmt);
