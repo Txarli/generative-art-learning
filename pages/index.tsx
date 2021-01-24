efae51c5cd4a398ef8eb0b3cdc72014e330fd1ad
Aitor Urrutia <aitor.urrutia@biko2.com>
2021-01-24 11:06:01 +0100

    Añade Head a Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,44 @@
+import React from 'react';
+import Head from 'next/head';
+
 function Test() {
   return (
-    <ul>
-      <li>
-        <a href="/tiled">Tiled</a>
-      </li>
-      <li>
-        <a href="/joy-division">Joy Division</a>
-      </li>
-      <li>
-        <a href="/triangular-mesh">Triangular mesh</a>
-      </li>
-      <li>
-        <a href="/cubic-disarray">Cubic disarray</a>
-      </li>
-      <li>
-        <a href="/cubic-disarray-tingling">Cubic disarray tingling</a>
-      </li>
-      <li>
-        <a href="/un-deux-trois">Un deux trois</a>
-      </li>
-      <li>
-        <a href="/un-deux-trois-rotating">Un deux trois rotating</a>
-      </li>
-      <li>
-        <a href="/circle-packing">Circle packing</a>
-      </li>
-      <li>
-        <a href="/circle-packing-borning">Circle packing borning</a>
-      </li>
-    </ul>
+    <>
+      <Head>
+        <title>Generative Art Notes | Aitor Urrutia</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <ul>
+        <li>
+          <a href="/tiled">Tiled</a>
+        </li>
+        <li>
+          <a href="/joy-division">Joy Division</a>
+        </li>
+        <li>
+          <a href="/triangular-mesh">Triangular mesh</a>
+        </li>
+        <li>
+          <a href="/cubic-disarray">Cubic disarray</a>
+        </li>
+        <li>
+          <a href="/cubic-disarray-tingling">Cubic disarray tingling</a>
+        </li>
+        <li>
+          <a href="/un-deux-trois">Un deux trois</a>
+        </li>
+        <li>
+          <a href="/un-deux-trois-rotating">Un deux trois rotating</a>
+        </li>
+        <li>
+          <a href="/circle-packing">Circle packing</a>
+        </li>
+        <li>
+          <a href="/circle-packing-borning">Circle packing borning</a>
+        </li>
+      </ul>
+    </>
   );
 }
 
